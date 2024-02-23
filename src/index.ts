4c26c29c63f61166fa055261bb879481004a913e
Shmuel Melamud <smelamud@redhat.com>
2024-02-23 03:47:49 +0200

    Serve the static UI.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import * as fs from 'fs';
 
 dotenv.config();
 
@@ -7,8 +8,13 @@ const app: Express = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Express + TypeScript Server");
+    let html = fs.readFileSync("views/index.html").toString();
+    const endpointUrl = `${req.protocol}://${req.hostname}/moera-push-relay/`;
+    html = html.replace("{{endpointUri}}", endpointUrl)
+    res.type("html").send(html);
 });
+
+app.use(express.static("public"));
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
