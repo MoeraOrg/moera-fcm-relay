9b66ed9a83ce963f5f9b80c5a297b7cf610b7142
Shmuel Melamud <smelamud@redhat.com>
2024-02-25 06:26:04 +0200

    Old Java code removed.

--- java/src/main/resources/db/migration/V2__clients_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE clients (
-    id uuid NOT NULL PRIMARY KEY,
-    client_id varchar(256) NOT NULL,
-    node_name varchar(128) NOT NULL,
-    lang varchar(8),
-    created_at timestamp without time zone NOT NULL
-);
-CREATE UNIQUE INDEX clients_client_id_idx ON clients(client_id);
-CREATE INDEX clients_node_name_idx ON clients(node_name);
